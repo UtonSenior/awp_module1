3cedfc80b2ef8341c9425e63342e5ef245de264d
UtonSenior <46914291+UtonSenior@users.noreply.github.com>
2020-09-27 18:39:47 -0500

    Just messing around

--- index.php
+++ index.php
@@ -1,13 +1,111 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+<meta name="Description" content="Enter your description here"/>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+<link rel="stylesheet" href="assets/css/style.css">
+<title>Module 1 - Uton Senior</title>
+<style>
+
+    body{
+        margin: 0px;
+        padding: 0px;
+    }
+    .styleh h4{
+        color: blue;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        border: 1px solid pink;
+        padding: 5px;
+    }
+    .styleh p{
+        font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+        padding: 5px;
+        border-left: 1px solid grey;
+        border-right: 1px solid grey;
+        border-bottom: 1px solid grey;
+    }
+
+    .styleh button{
+        margin-top: 5px; 
+        color: red;
+    }
+</style>
 </head>
 <body>
-    <?php
-        echo 'Hello World, Welcome to another Lesson in PHP.';
-    ?>
+
+    <div class="container">
+
+        <h2>
+            <?php
+                echo 'Hello World, Welcome to another Lesson in PHP.';
+            ?>
+        </h2>  
+
+        <div class="row styleh">
+            <div class="col-sm-4">
+                <h4>Happy Times</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                    <br>
+                    <button class="btn btn-info">Read More >>></button>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4>Sad Memories</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora repellat quam eaque blanditiis provident enim alias sequi ipsam molestias cum, itaque obcaecati laboriosam pariatur illum quos accusantium quisquam voluptate sunt adipisci error in ducimus delectus ullam! Non aperiam officiis laborum.
+                    <br>
+                    <button class="btn btn-info">Read More >>></button>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4>The Experiences</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                    <br>
+                    <button class="btn btn-info">Read More >>></button>
+                </p>
+            </div>
+
+        </div>
+
+        <div class="row styleh">
+            <div class="col-sm-4">
+                <h4>The Moments</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                    <br>
+                    <button class="btn btn-info">Read More >>></button>
+                </p>
+            </div>
+            
+            <div class="col-sm-4">
+                <h4>Long Lasting</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                    <br>
+                    <button class="btn btn-info">Read More >>></button>
+                </p>
+            </div>
+            
+            <div class="col-sm-4">
+                <h4>Togetherness</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora repellat quam eaque blanditiis provident enim alias sequi ipsam molestias cum, itaque obcaecati laboriosam pariatur illum quos accusantium quisquam voluptate sunt adipisci error in ducimus delectus ullam! Non aperiam officiis laborum.
+                    <br>
+                    <button class="btn btn-info">Read More >>></button>
+                </p>
+            </div>
+        </div>
+        
+    </div>
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </body>
 </html>
