4025f7138d2041a5900f2e4396d167d18d56a59b
UtonSenior <46914291+UtonSenior@users.noreply.github.com>
2020-09-27 19:20:32 -0500

    Updated layout.

--- index.php
+++ index.php
@@ -20,6 +20,8 @@
         font-family: Georgia, 'Times New Roman', Times, serif;
         border: 1px solid pink;
         padding: 5px;
+
+        margin: 15px;
     }
     .styleh p{
         font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
@@ -27,6 +29,8 @@
         border-left: 1px solid grey;
         border-right: 1px solid grey;
         border-bottom: 1px solid grey;
+
+        margin: 15px;
     }
 
     .styleh button{
@@ -47,60 +51,71 @@
 
         <div class="row styleh">
             <div class="col-sm-4">
-                <h4>Happy Times</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
-                    <br>
-                    <button class="btn btn-info">Read More >>></button>
-                </p>
+                <div class="row">
+                    <div class="col">
+                    <h4>Happy Times</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                            <br>
+                            <button class="btn btn-info">Read More >>></button>
+                        </p>
+                    </div>
+                    <div class="col">
+                    <h4>The Moments</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                            <br>
+                            <button class="btn btn-info">Read More >>></button>
+                        </p>
+                    </div>
+                </div>                
             </div>
             <div class="col-sm-4">
-                <h4>Sad Memories</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora repellat quam eaque blanditiis provident enim alias sequi ipsam molestias cum, itaque obcaecati laboriosam pariatur illum quos accusantium quisquam voluptate sunt adipisci error in ducimus delectus ullam! Non aperiam officiis laborum.
-                    <br>
-                    <button class="btn btn-info">Read More >>></button>
-                </p>
+                <div class="row">
+                    <div class="col">
+                        <h4>Sad Memories</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora repellat quam eaque blanditiis provident enim alias sequi ipsam molestias cum, itaque obcaecati laboriosam pariatur illum quos accusantium quisquam voluptate sunt adipisci error in ducimus delectus ullam! Non aperiam officiis laborum.
+                            <br>
+                            <button class="btn btn-info">Read More >>></button>
+                        </p>
+
+                        <div class="col">
+                            <h4>Long Lasting</h4>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                                <br>
+                                <button class="btn btn-info">Read More >>></button>
+                            </p>
+                    </div>
+                    </div>
+                </div>
             </div>
             <div class="col-sm-4">
-                <h4>The Experiences</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
-                    <br>
-                    <button class="btn btn-info">Read More >>></button>
-                </p>
+                <div class="row">
+                    <div class="col">
+                        <h4>Togetherness</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                            <br>
+                            <button class="btn btn-info">Read More >>></button>
+                        </p>
+                    </div>
+
+                    <div class="col">
+                        <h4>The Experiences</h4>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
+                            <br>
+                            <button class="btn btn-info">Read More >>></button>
+                        </p>
+                    </div>
+                </div>
             </div>
 
         </div>
 
-        <div class="row styleh">
-            <div class="col-sm-4">
-                <h4>The Moments</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
-                    <br>
-                    <button class="btn btn-info">Read More >>></button>
-                </p>
-            </div>
-            
-            <div class="col-sm-4">
-                <h4>Long Lasting</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam animi distinctio nemo, temporibus aspernatur ducimus voluptatem hic quae eum, eveniet eius itaque officia totam a quos excepturi ab quisquam unde?
-                    <br>
-                    <button class="btn btn-info">Read More >>></button>
-                </p>
-            </div>
-            
-            <div class="col-sm-4">
-                <h4>Togetherness</h4>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora repellat quam eaque blanditiis provident enim alias sequi ipsam molestias cum, itaque obcaecati laboriosam pariatur illum quos accusantium quisquam voluptate sunt adipisci error in ducimus delectus ullam! Non aperiam officiis laborum.
-                    <br>
-                    <button class="btn btn-info">Read More >>></button>
-                </p>
-            </div>
-        </div>
+        
         
     </div>
 
